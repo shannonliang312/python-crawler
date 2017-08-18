e7db96d4de3d7d746c2d4963f17da26e182252aa
Shannon Liang <shannonliang312@gmail.com>
2017-08-18 13:36:32 +0800

    update

---
+++ phub.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+s = requests
+url = 'https://www.pornhub.com'
+
+proxy = {
+    "http": "http://127.0.0.1:1080"
+}
+
+result = s.get(url=url, proxies=proxy)
+content = result.content
+res = BeautifulSoup(content, 'lxml')
+
+print(res.prettify())
